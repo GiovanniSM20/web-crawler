070b3a70eb252f720b91f1d14b1bdf741847f6c7
GiovanniSM20 <giovannimartins2000@gmail.com>
2016-07-26 14:49:59 -0300

    0.0.1

---
+++ web-crawler-1/crawler.js
@@ -0,0 +1,7 @@
+// Request is used to make HTTP requests.
+//Cheerio is used to parse and select HTML elements on the page.
+// URL is used to parse URLs.
+
+var request = require ( 'pedido' );
+var cheerio = require ( 'cheerio' );
+var URL = require ( 'url-parse' );
